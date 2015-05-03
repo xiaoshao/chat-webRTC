538e171355c228c6810faa17c9a08734068006e5
Shao Zengwei <zengwei19861029@163.com>
2015-05-03 19:29:13 +0800

    Text communicate. Zengwei Shao

--- app.js
+++ app.js
@@ -16,3 +16,27 @@ app.get("/", function(req, resp){
 
 var websocketServer = require("ws").Server;
 
+var wss = new websocketServer({server:server});
+
+var clients = [];
+wss.on("connection", function(client){
+    console.log(client + "connected.");
+
+    clients.push(client);
+
+    client.on("message", function(message){
+        console.log("received:" + message);
+        sendMessage(message);
+    });
+});
+
+function sendMessage(message){
+    for(var index =0;  index < clients.length; index ++){
+        var cls = clients[index];
+        cls.send(message, function(error){
+            if(error){
+                console.log("send to client error." + error);
+            }
+        });
+    }
+}
