b79a50872ea82ca62af712659cf4b4d1bd54ef91
Shao Zengwei <zengwei19861029@163.com>
2015-06-07 21:26:45 +0800

    server send message to clients

--- app.js
+++ app.js
@@ -6,30 +6,44 @@ var express = require("express");
 var app = express();
 var server = require('http').createServer(app);
 
-server.listen(3000, function(){
+server.listen(3000, function () {
     console.log("Listening on 3000");
 });
 
-app.get("/", function(req, resp){
-   resp.sendFile(__dirname + "/index.html");
+app.get("/", function (req, resp) {
+    resp.sendFile(__dirname + "/index.html");
 });
 
 var websocketServer = require("ws").Server;
 
-var wss = new websocketServer({server:server});
+var wss = new websocketServer({server: server});
 
 var clients = [];
-wss.on("connection", function(client){
+wss.on("connection", function (client) {
     console.log("connect....");
     clients.push(client);
-    client.send("{type: 'textMessage', data: { message: 'hello world!'}}", function(error){
-        if(error){
+    client.send("{type: 'textMessage', data: { message: 'hello world!'}}", function (error) {
+        if (error) {
             console.log("send message error : " + error);
         }
     });
 
     client.onmessage = function (msg) {
-        
-        console.log(JSON.stringify(msg));
+        //When get message from client
+        console.log(msg.data);
+        sendMessage(msg, client);
+
     };
+
 });
+
+//send message to clients without self
+function sendMessage(msg, client) {
+
+    for (cli in clients) {
+        if (cli === client) {
+            continue;
+        }
+        cli.send(msg);
+    }
+}
